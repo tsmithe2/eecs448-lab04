ee8292b662bd199045afda6b68c8d8b23727cb81
Thomas <thomas_smithey@yahoo.com>
2019-03-05 17:00:13 -0600

    finished multTable.php

--- multTable.php
+++ multTable.php
@@ -1,14 +1,29 @@
 <?php
-	echo "<p>Hello World!</p>"
-   /* echo "<table>"
-    for ($i = 1; $i < 100; $i++)
+    echo "<table border = 1>";
+    $counter = 0;
+    for ($i = 0; $i < 101; $i++)
     {
-        echo "<tr>" . $i;
-        for ($j = 1; $j < 100; $j++)
-        {
-            echo "<td>" . $j . "</td>";
-        }
-        echo "</tr>";
+	echo "<tr>";
+	for ($j = 0; $j < 101; $j++)
+	{
+		if ($counter == 0)
+		{
+			echo "<td>" . $j . "</td>";
+		}
+		else
+		{
+			if ($j == 0)
+			{
+				echo "<td>" . (($j + 1) * $counter) . "</td>";
+			}
+			else
+			{
+				echo "<td>" . ($j * $counter) . "</td>";
+			}
+		}
+	}
+	$counter++;
+	echo "</tr>";
     }
-    echo "</table>" */
+    echo "</table>"; 
 ?>
